30c6f93caf5602416c6f73d6265f85db32082445
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-05-31 14:25:25 +0200

    added  contact page button from home

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -9,11 +9,12 @@ export default function Home() {
         <div className=" flex flex-col items-start gap-y-6 p-10">
           <div className=" flex flex-col gap-2 items-start ">
             <HomeNavButton href={'./projects'}>Projects</HomeNavButton>
-            <HomeNavButton href={'./about'}>About Me</HomeNavButton>
             <HomeNavButton href={'./articles'}>Articles</HomeNavButton>
+            <HomeNavButton href={'./about'}>About</HomeNavButton>
+            <HomeNavButton href={'./contact'}>Contact</HomeNavButton>
           </div>
           <div className=" flex gap-3 px-5 justify-end w-full">
-          <p>{'-->'}</p><Link href={'./about'}>   Nicklas Båkind-Øverjordet</Link>
+          <Link href={'./about'}>   Nicklas Båkind-Øverjordet</Link><p>{'-->'}</p>
           </div>
         </div>
       </div>
