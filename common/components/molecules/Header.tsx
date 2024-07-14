5e41834fb0dec59c1c43c59330340fe80f02625b
enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-14 18:10:20 +0200

    added published at text in article

--- common/components/molecules/Header.tsx
+++ common/components/molecules/Header.tsx
@@ -1,12 +1,16 @@
 import type { headerType } from "@/common/types/molecules/header.type";
 import Entry from "../atom/Entry";
 import { TextFild } from "../wrapper/ContentWrapper";
+import { Calendar } from "lucide-react";
 
 const Header = (props: headerType) => {
 	return (
 		<TextFild>
 			<h1>{props.title}</h1>
-			<p>{props.publishedAt}</p>
+			<div className=" flex gap-2 items-center">
+				<Calendar />
+				<p>{props.publishedAt}</p>
+			</div>
 			<p className=" entry">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. In, cupiditate
 				omnis amet non totam laborum. Qui voluptate quasi cum, sapiente tempora
