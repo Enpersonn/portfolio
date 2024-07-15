427f02d7562f1ce82c94f1ef1df9074a1136e222
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-15 13:01:27 +0200

    Removed CardSkeleton and made new skeleton for article fild in article list

--- common/components/organism/ArticleList.skeleton.tsx
+++ common/components/organism/ArticleList.skeleton.tsx
@@ -1,10 +1,20 @@
-import { CardSkeleton } from "../wrapper/Card";
+import Card from "../wrapper/Card";
+
+export const ArticleListFildSkeleton = () => (
+	<Card>
+		<div className="animate-pulse rounded-[15px] aspect-[16/9] w-[550px] bg-[#ffffff1c]" />
+		<div className=" p-4">
+			<div className="animate-pulse h-4 bg-[#ffffff1c] w-3/4 mb-2" />
+			<div className="animate-pulse h-4 bg-[#ffffff1c] w-1/2" />
+		</div>
+	</Card>
+);
 
 const ArticleListSkeleton = () => (
-	<div className=" grid gap-10 grid-cols-3">
-		<CardSkeleton />
-		<CardSkeleton />
-		<CardSkeleton />
+	<div className=" grid gap-10 md:grid-cols-3">
+		<ArticleListFildSkeleton />
+		<ArticleListFildSkeleton />
+		<ArticleListFildSkeleton />
 	</div>
 );
 
--- common/components/wrapper/Card.tsx
+++ common/components/wrapper/Card.tsx
@@ -8,14 +8,4 @@ const Card = styled.div`
 
     `;
 
-export const CardSkeleton = () => (
-	<Card>
-		<div className="animate-pulse rounded-[15px] h-48 bg-[#ffffff1c]" />
-		<div className=" p-4">
-			<div className="animate-pulse h-4 bg-[#ffffff1c] w-3/4 mb-2" />
-			<div className="animate-pulse h-4 bg-[#ffffff1c] w-1/2" />
-		</div>
-	</Card>
-);
-
 export default Card;
