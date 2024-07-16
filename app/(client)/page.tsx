31e8db441f2c459bc493d5b50c4ebef6a6313c5a
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-16 09:23:11 +0200

    moved Home page content to home view

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,23 +1,6 @@
 "use client";
-import HomeNavButton from "@/common/components/atom/HomeNavButton";
-import Link from "next/link";
+import HomeView from "@/common/components/views/Home.view";
 
 export default function Home() {
-  return (
-    <main className=" w-[100vw] h-[100vh]">
-      <div className=" flex items-end justify-end w-full h-full">
-        <div className=" flex flex-col items-start gap-y-6 p-10">
-          <div className=" flex flex-col gap-2 items-start ">
-            <HomeNavButton href={'./projects'}>Projects</HomeNavButton>
-            <HomeNavButton href={'./articles'}>Articles</HomeNavButton>
-            <HomeNavButton href={'./about'}>About</HomeNavButton>
-            <HomeNavButton href={'./contact'}>Contact</HomeNavButton>
-          </div>
-          <div className=" flex gap-3 px-5 justify-end w-full">
-          <Link href={'./about'}>   Nicklas Båkind-Øverjordet</Link><p>{'-->'}</p>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+	return <HomeView />;
 }
--- common/components/views/Home.view.tsx
+++ common/components/views/Home.view.tsx
@@ -1,8 +1,26 @@
+import Link from "next/link";
+import HomeNavButton from "../atom/HomeNavButton";
+import { Suspense } from "react";
 
 export default function HomeView() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+	return (
+		<main className=" w-[100vw] h-[100vh]">
+			<div className=" flex items-end justify-end w-full h-full">
+				<div className=" flex flex-col items-start gap-y-6 p-10">
+					<div className=" flex flex-col gap-2 items-start ">
+						<Suspense fallback={<div>Loading...</div>}>
+							<HomeNavButton href={"./projects"}>Projects</HomeNavButton>
+							<HomeNavButton href={"./articles"}>Articles</HomeNavButton>
+							<HomeNavButton href={"./about"}>About</HomeNavButton>
+							<HomeNavButton href={"./contact"}>Contact</HomeNavButton>
+						</Suspense>
+					</div>
+					<div className=" flex gap-3 px-5 justify-end w-full">
+						<Link href={"./about"}> Nicklas Båkind-Øverjordet</Link>
+						<p>{"-->"}</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
