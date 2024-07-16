212f920971fe5a12be6c71ed2d9d93f721e7b4b7
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-16 15:56:08 +0200

    Updated PageHeader

--- common/components/organism/PageHeader.tsx
+++ common/components/organism/PageHeader.tsx
@@ -1,37 +1,40 @@
-import { House } from "lucide-react";
+import { ArrowLeft, House } from "lucide-react";
 import Link from "next/link";
-import styled from "styled-components";
 import PageNavigation from "../molecules/PageNavigation";
+import { useRouter } from "next/navigation";
 
-const NavButton = styled(Link)`
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 28px;
-    height: 28px;
-    border: 1px solid #86868663;
-    `;
-
-const PageHeader = () => (
-	<nav className="  w-full h-[48px] grid grid-cols-12 items-center  border-b-[1px] border-[#86868663]">
-		<div className=" col-start-3 flex gap-3 ">
-			<NavButton href={"../"}>
-				<House size={18} />
-			</NavButton>
-		</div>
-		<div className=" col-span-6 flex justify-center">
-			<PageNavigation
-				Pages={[
-					{ title: "Projects", href: "../projects" },
-					{ title: "Articles", href: "../articles/discover" },
-					{ title: "About", href: "../about" },
-					{ title: "Contact", href: "../contact" },
-				]}
-				size="sm"
-			/>
-		</div>
-	</nav>
-);
+const PageHeader = () => {
+	const router = useRouter();
+	return (
+		<nav className="  w-full h-[48px] grid grid-cols-12 items-center  border-b-[1px] border-[#86868663]">
+			<div className=" col-start-3 flex gap-3 ">
+				<button
+					type="button"
+					className=" rounded-full size-[28px] flex items-center justify-center border-[1px] border-[#86868663] "
+					onClick={() => router.back()}
+				>
+					<ArrowLeft size={18} />
+				</button>
+				<Link
+					href={"../"}
+					className=" rounded-full size-[28px] flex items-center justify-center border-[1px] border-[#86868663] "
+				>
+					<House size={18} />
+				</Link>
+			</div>
+			<div className=" col-span-6 flex justify-center">
+				<PageNavigation
+					Pages={[
+						{ title: "Projects", href: "../projects" },
+						{ title: "Articles", href: "../articles/discover" },
+						{ title: "About", href: "../about" },
+						{ title: "Contact", href: "../contact" },
+					]}
+					size="sm"
+				/>
+			</div>
+		</nav>
+	);
+};
 
 export default PageHeader;
