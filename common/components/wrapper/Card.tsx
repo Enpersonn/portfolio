9d89297ba578a8f0bb13e222f2b2ea7bfcc3acce
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-16 13:00:10 +0200

    updated card style

--- common/components/wrapper/Card.tsx
+++ common/components/wrapper/Card.tsx
@@ -1,5 +1,5 @@
 const Card = ({ children }: { children: React.ReactNode }) => (
-	<div className=" rounded-[15px] overflow-hidden border-[1px] border-slate-600 w-full h-full">
+	<div className=" rounded-[15px] overflow-hidden border-[1px] border-slate-600/50 w-full h-full">
 		{children}
 	</div>
 );
