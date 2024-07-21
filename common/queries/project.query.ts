ffabe212cbadbde074c70282b33070ba0883d24f
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-21 20:33:24 +0200

    updated project query

--- common/queries/project.query.ts
+++ common/queries/project.query.ts
@@ -1,15 +1,23 @@
 import { groq } from "next-sanity";
 
 const PROJECT_QUERY = groq`
-*[_type == "project"] | order(publishedAt desc) {
+*[_type == "project" && slug.current == $slug ] | order(publishedAt desc) {
+  
+  _type,
+  _createdAt,
+  _updatedAt,
+  _id,
+  "slug": slug.content,
   
   "header": {
   title,
-  slug,
   mainImage,
-  projectLink
+  projectLink,
   publishedAt,
   entry,
+  },
+  "body": {
+    content,
   }
 
 }
