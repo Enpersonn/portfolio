960f2ae1f671ac210e22865462c5fdf3202de6f6
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-23 14:14:55 +0200

    updated the tabke of content component

--- common/components/molecules/TableOfContent.tsx
+++ common/components/molecules/TableOfContent.tsx
@@ -4,29 +4,49 @@ import Link from "next/link";
 const TableOfContent = ({ headlines }: tableOfContentType) => {
 	return (
 		<div>
-			<h2>Table of content</h2>
-			{headlines?.map((headline, index) => {
-				switch (headline.style) {
-					case "h2":
-						return (
-							<Link key={headline._key} href={`#${headline.children[0].text}`}>
-								<h2>{headline.children[0].text}</h2>
-							</Link>
-						);
-					case "h3":
-						return (
-							<div className=" pl-5">
+			<div className=" w-full border-b-[1px] border-white">
+				<h2>Table of content</h2>
+			</div>
+			<div className=" pl-5">
+				{headlines?.map((headline, index) => {
+					switch (headline.style) {
+						case "h2":
+							return (
 								<Link
 									key={headline._key}
 									href={`#${headline.children[0].text}`}
-									className=" pl-2  border-l block"
+									className=" hover:underline"
 								>
-									<h3>{headline.children[0].text}</h3>
+									<h2>{headline.children[0].text}</h2>
 								</Link>
-							</div>
-						);
-				}
-			})}
+							);
+						case "h3":
+							return (
+								<div className=" pl-5">
+									<Link
+										key={headline._key}
+										href={`#${headline.children[0].text}`}
+										className=" pl-2  border-l block hover:underline"
+									>
+										<h3>{headline.children[0].text}</h3>
+									</Link>
+								</div>
+							);
+						case "h4":
+							return (
+								<div className=" pl-5">
+									<Link
+										key={headline._key}
+										href={`#${headline.children[0].text}`}
+										className=" pl-2  border-l block hover:underline"
+									>
+										<h4>{headline.children[0].text}</h4>
+									</Link>
+								</div>
+							);
+					}
+				})}
+			</div>
 		</div>
 	);
 };
