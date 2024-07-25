9b598bb1c77883b26639a8f24d79ed331df17c0b
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-07-25 12:04:45 +0200

    updated quote component

--- common/components/molecules/portableText/PTCustomeComponents.tsx
+++ common/components/molecules/portableText/PTCustomeComponents.tsx
@@ -16,13 +16,14 @@ const type: {
 	),
 
 	quote: (props) => (
-		<blockquote className=" my-10">
-			<Quote size={64} className=" hidden md:block" />
-			<div>
-				<p className=" quote-text">{props.value.quote}</p>
-				<p className=" quote-author">- {props.value.author}</p>
-			</div>
-		</blockquote>
+		<div className="grid grid-cols-8 my-10">
+			<blockquote className=" col-start-2 col-span-6">
+				<div>
+					<p className=" quote-text">{props.value.quote}</p>
+					<p className=" quote-author">- {props.value.author}</p>
+				</div>
+			</blockquote>
+		</div>
 	),
 
 	highlighted: (props) => (
