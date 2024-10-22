7636d674c4e75cddf8296290b6682e70d6d79337
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-10-22 18:20:47 +0200

    Updated DocsNav.tsx

--- common/components/molecules/DocsNav.tsx
+++ common/components/molecules/DocsNav.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 
 const DocsNav = ({ docs }: { docs: NavPageType[] }) => {
 	return (
-		<div className=' flex flex-col gap-5 h-fit min-h-[500px] col-span-2 p-5 border-[1px] border-shadow-gray rounded-lg'>
+		<div className=' flex flex-col gap-5 h-fit min-h-[500px] col-start-2 col-span-2 p-5 border-[1px] border-shadow-gray rounded-lg'>
 			<h2 className=' text-2xl font-bold'>Docs</h2>
-			<div className='w-full'>
+			<button type='button' className='w-full flex flex-col gap-1 text-left'>
 				<Link href={'/docs/hooks'}>hooks</Link>
 				<div className=' ml-1 flex flex-col gap-1 border-l-2 border-l-shadow-gray pl-1'>
 					{docs
@@ -18,7 +18,7 @@ const DocsNav = ({ docs }: { docs: NavPageType[] }) => {
 							</Link>
 						))}
 				</div>
-			</div>
+			</button>
 		</div>
 	);
 };
